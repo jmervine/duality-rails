5bf8533561937be24a791cb352271f5f8ee521f7
Joshua Mervine <joshua@mervine.net>
2012-07-21 17:52:07 -0400

    initial implementation

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source :rubygems
+gem 'duality'
+
+group :development do
+  gem 'activesupport' # should be included with tails
+end
+group :test do
+  gem 'rspec'
+  gem 'simplecov'
+end
+
---
+++ lib/duality_rails.rb
@@ -0,0 +1,56 @@
+require 'active_support'
+require 'duality'
+
+class DualityRails < ActiveSupport::Cache::Store
+  include Duality
+
+  # Active Support methods:
+  #
+  # cleanup
+  def cleanup(*args)
+    flush_expired(*args)
+  end
+
+  # clear
+  def clear(*args)
+    flush
+  end
+
+  # decrement 
+  # - supported through Duality::missing_method
+
+  # delete
+  # - implemented in duality
+
+  # delete_matched
+  # - supported through Duality::missing_method
+
+  # exists?
+  # - supported through Duality::missing_method
+
+  # fetch
+  # - inherited from ActiveSupport
+
+  # increment
+  # - supported through Duality::missing_method
+
+  # mute
+  # - inherited from ActiveSupport
+
+  # read
+  def read(*args)
+    get(*args)
+  end
+
+  # read_multi
+  # - inherited from ActiveSupport
+
+  # silence!
+  # - inherited from ActiveSupport
+  
+  # write
+  def write(*args)
+    get(*args)
+  end
+
+end
